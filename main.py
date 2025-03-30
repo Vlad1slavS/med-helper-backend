4d7b876dcd3602504c207f1feec555afe0fa344d
vladstepanov <xstep759@gmail.com>
2025-03-30 23:13:03 +0900

    second

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from voice_message_process import voice_controller
 from text_message_proccess import text_controller
+from fastapi.responses import JSONResponse
+from datetime import datetime
 
 app = FastAPI(title="Медицинский анализатор")
+
+@app.get("/api")
+async def get_status():
+    current_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")  # Текущее время в формате UTC
+    return JSONResponse(content={"status": "ok", "time": current_time})
 
 # Подключаем роутеры с префиксом "/api"
 app.include_router(voice_controller.router, prefix="/api")
